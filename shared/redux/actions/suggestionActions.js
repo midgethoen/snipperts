cf576df32dbd46970d5db5ee25308bcd51a94bb2
Lucas Feliciano <mail@lucasfeliciano.com>
2016-04-09 06:04:48 +0200

    added suggestions actions and reducer

---
+++ shared/redux/actions/suggestionActions.js
@@ -0,0 +1,20 @@
+import * as ActionTypes from '../constants/constants';
+import Config from '../../../server/config';
+import fetch from 'isomorphic-fetch';
+
+const baseURL = typeof window === 'undefined' ? process.env.BASE_URL || (`http://localhost:${Config.port}`) : '';
+
+export function replaceSuggestions(suggestions) {
+  return {
+    type: ActionTypes.REPLACE_SUGGESTIONS,
+    suggestions,
+  };
+}
+
+export function fetchSuggestions() {
+  return (dispatch) => {
+    return fetch(`${baseURL}/api/suggestions`).
+      then((response) => response.json()).
+      then((response) => dispatch(replaceSuggestions(response.suggestions)));
+  };
+}
---
+++ shared/redux/reducers/suggestionReducer.js
@@ -0,0 +1,18 @@
+import * as ActionTypes from '../constants/constants';
+
+const initialState = { suggestions: [] };
+
+const suggestionReducer = (state = initialState, action) => {
+  const { type, suggestions } = action;
+  switch (type) {
+    case ActionTypes.REPLACE_SUGGESTIONS :
+      return {
+        suggestions,
+      };
+
+    default:
+      return state;
+  }
+};
+
+export default suggestionReducer;
