c6c46d3105afea66ebe7e41c476495d32d6672a2
Lucas Feliciano <mail@lucasfeliciano.com>
2016-04-09 19:37:11 +0200

    added User and Topic actions

--- shared/redux/constants/index.js
+++ shared/redux/constants/index.js
@@ -7,10 +7,18 @@ export const ADD_SNIPPET_REQUEST     = 'ADD_SNIPPET_REQUEST';
 export const ADD_SELECTED_SNIPPET    = 'ADD_SELECTED_SNIPPET';
 export const DELETE_SNIPPET          = 'DELETE_SNIPPET';
 export const REPLACE_SNIPPETS        = 'REPLACE_SNIPPETS';
-export const REPLACE_USERS           = 'REPLACE_USERS';
-export const REPLACE_TOPICS          = 'REPLACE_TOPICS';
 
 /**
- * Suggestions
+ * Suggestion
  */
 export const REPLACE_SUGGESTIONS = 'REPLACE_SUGGESTIONS';
+
+/**
+ * User
+ */
+export const REPLACE_USERS = 'REPLACE_USERS';
+
+/**
+ * Topic
+ */
+export const REPLACE_TOPICS = 'REPLACE_TOPICS';
