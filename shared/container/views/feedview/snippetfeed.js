608c6c93b3556414fd9fca72fb854e57d50af8d4
Midge 't Hoen <midgethoen@gmail.com>
2016-04-14 01:07:01 +0200

    change to groupWith for grouping

--- shared/container/views/feedview/snippetfeed.js
+++ shared/container/views/feedview/snippetfeed.js
@@ -1,31 +1,20 @@
 import React, { PropTypes } from 'react';
 import SnippetGroup from './snippetgroup';
-import { splitWhen } from 'ramda';
-
-function snippetDayOrUserChanged() {
-  let day;
-  let user;
-  return function checkIfDayOrUserChanged(snippet) {
-    if (
-      day !== undefined && user !== undefined &&
-      day === new Date(snippet.createdAt).getDate() &&
-      user === snippet.user
-    ) {
-      return false;
-    }
-    day = new Date(snippet.createdAt).getDate();
-    user = snippet.user;
-    return true;
-  };
-}
+import { groupWith } from 'ramda';
 
 function SnippetFeed({ snippets }) {
   return (
     <section className="row feed">
       <div className="col-md-9 col-md-offset-3 col-lg-6">
         {
-          splitWhen(snippetDayOrUserChanged(), snippets)
-            .map(snippetGroup => <SnippetGroup snippets={snippetGroup} />)
+          groupWith(
+            (s1, s2) =>
+              s1.user === s2.user &&
+              new Date(s1.createdAt).getDate() === new Date(s2.createdAt).getDate(),
+            snippets)
+            .map(
+              snippetGroup => <SnippetGroup snippets={snippetGroup} />
+            )
         }
       </div>
     </section>
